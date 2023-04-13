2ec079bee2772fe34daef8d1951a5593fc565253
tanguy deleage <tanguy.deleage0@gmail.com>
2023-04-13 12:30:02 +0200

    Add navigation

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -9,6 +9,10 @@
         {{ project.kpi }}
         <a href="{{ project.url }}">Learn more</a>
     </div>
+
+    <div v-for="(project, index) in projects" :key="project">
+        <a href="">{{ index }}</a>
+    </div>
 </template>
    
    <script>
@@ -30,7 +34,7 @@
                 description: "Built through Le Wagon Bootcamp (leader in tech bootcamp) with 2 other people. Matematch is a Web App helping foreign students to meet locals and discover local culture.",
                 kpi: [1,2,3],
                 url: "des trucs",
-                image: "https://www.tanguydeleage.com/assets/img/projects/matematch/matematch-mockup.png"},
+                image: "https://www.tanguydeleage.com/assets/img/projects/coop/coop-mockup.png"},
             ]
         }
      }
