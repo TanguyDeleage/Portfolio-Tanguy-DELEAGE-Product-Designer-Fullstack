84706e477084611f38ee35e7dc35f0bf587953e9
tanguy deleage <tanguy.deleage0@gmail.com>
2023-04-15 18:13:31 +0200

    Css mainly done for desktop in contact

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,24 +1,26 @@
 <template>
-    <div>
-        <h1>Contact</h1>
-        <div v-for="contact in contacts" :key="contact">
-            {{ contact.title }}
-            <span>{{ contact.icon }} {{ contact.link }}</span>
+    <div class="contact">
+        <div class="socials background">
+            <h1>Contact</h1>
+            <div v-for="contact in contacts" :key="contact" class="social-links">
+                {{ contact.title }}
+                <span class="social-link">{{ contact.icon }}{{ contact.link }}</span>
+            </div>
+
+            <div>
+                <span> <img src="" alt="X">Tanguy Deléage</span>
+                <p>Feel free to share your thoughts and questions. I’m always open to exchange about new projects.</p>
+            </div>
         </div>
 
-        <div>
-            <span> <img src="" alt="X">Tanguy Deléage</span>
-            <p>Feel free to share your thoughts and questions. I’m always open to exchange about new projects.</p>
-        </div>
-    </div>
+        <form class="contact-form">
+            <div v-for="input in inputs" :key="input" class="form-input">
+                {{ input.title }}
+                <input type="text" :placeholder= "input.placeholder">
+            </div>
 
-    <div>
-        <form v-for="input in inputs" :key="input">
-            {{ input.title }}
-            <input type="text" :placeholder= "input.placeholder">
+            <a href="" class="btn-primary">Submit</a>
         </form>
-
-        <button>Submit</button>
     </div>
  </template>
     
@@ -65,9 +67,52 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-ul {
-    list-style-type: none;
-    padding: 0;
+.contact {
+    display: flex;
+    flex-direction: row;
+    height: calc(100vh - 5rem);
+}
+
+.socials {
+    text-align: left;
+    padding-left: 4rem;
+    width: 40vw;
+}
+
+.contact-form {
+    width: 60vw;
+    display: flex;
+    flex-direction: column;
+    padding: 4rem 4rem;
+    justify-content: center;
+    align-items: center;
+}
+
+.form-input {
+    width: 100%;
+}
+
+.contact-form input {
+    background-color: var(--secondary);
+    border: #CACACA 1px solid;
+    box-shadow: 4px 4px 19px 1px #E3E3E3;
+    border-radius: 0.5rem;
+    width: 100%;
+    padding: 2rem 1rem;
+}
+::placeholder {
+    color: hsla(0, 0%, 70%, 1);
+}
+
+.social-links {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2rem;;
+}
+
+.social-link {
+   display: flex;
+   flex-direction: row; 
 }
 </style>
     
