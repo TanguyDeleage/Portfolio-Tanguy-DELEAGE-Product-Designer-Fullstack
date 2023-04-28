338b46b9274f4c35b0e11c6b92e4eea9be50b3f3
tanguy deleage <tanguy.deleage0@gmail.com>
2023-04-28 21:49:23 +0200

    Fix secondary button at hero

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -13,7 +13,7 @@
           <div class="cta">
             <div class="btn-primary" @click="toPortfolio()" href="">View Portfolio</div>
             <!-- <a class="btn-secondary" href="/contact">Contact</a> -->
-            <router-link class="btn-primary" to="/contact">Contact</router-link>
+            <router-link class="btn-secondary" to="/contact">Contact</router-link>
           </div>
             
         </div>
