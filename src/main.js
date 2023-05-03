80d0b70b373710d73a7070d9f424e36066285e5c
tanguy deleage <tanguy.deleage0@gmail.com>
2023-05-03 20:00:21 +0200

    trying to deploy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+// createApp(App).use(router).mount('#app')
+
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: function (h) { return h(App) }
+}).$mount('#app')
