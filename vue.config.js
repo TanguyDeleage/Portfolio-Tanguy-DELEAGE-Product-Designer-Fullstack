b5849ce88203dfa9cd8a153bfda729610382da70
tanguy deleage <tanguy.deleage0@gmail.com>
2023-05-12 19:09:23 +0200

    Adding robot.txt and sitemap

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,36 @@
 // })
 
 module.exports = {
-  // publicPath: '../dist'
   publicPath: '/'
 };
+
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+const paths = [
+  {
+      path: '/',
+      lastmod: '2023-05-12',
+      priority: 1.0,
+      changefreq: 'yearly'
+  },
+  {
+      path: '/about/',
+      lastmod: '2023-05-12',
+      priority: 0.9,
+      changefreq: 'yearly'
+  },
+  {
+    path: '/contact/',
+    lastmod: '2023-05-12',
+    priority: 0.9,
+    changefreq: 'yearly'
+  }
+]
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new SitemapPlugin({ base: 'https://tanguydeleage.com', paths })
+        ]
+    },
+    // Other exports here
+}
